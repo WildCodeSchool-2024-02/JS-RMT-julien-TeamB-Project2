0222fae927faf89e573724514551eda5871791e9
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-05-22 16:42:35 +0200

    debut css composant cart

--- client/src/components/CartComponent.jsx
+++ client/src/components/CartComponent.jsx
@@ -1,12 +1,12 @@
-
+import "./CartComponent.css"
 
 function CartComponent({game}) {
   return (
     <section className='cartContainer'>
       <img src={game.image} alt={game.title} />
       <h2>{game.title}</h2>
-      <span>Quantité(s) 1</span>
-      <span>Prix {game.price} EUR</span>
+      <p>Quantité(s) 1</p>
+      <p>Prix {game.price} EUR</p>
     </section>
   )
 }
