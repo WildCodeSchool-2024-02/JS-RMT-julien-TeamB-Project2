4c81d024f40dc326727d3c46d35905cf41672ea3
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-05-22 11:57:26 +0200

    Modification PR (router.js)

--- server/app/router.js
+++ server/app/router.js
@@ -1,26 +1,14 @@
 const express = require("express");
 
-const games = require('../database/data')
-
 const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
-
-// Import itemActions module for handling item-related operations
-// const itemActions = require("./controllers/itemActions");
-
-// Route to get a list of items
+// Route to get a list of games
 router.get("/games", (req, res) => {
-  res.status(200).json(games);
+  res.send("Hello");
 });
-
-// Route to get a specific item by ID
-// router.get("/items/:id", itemActions.read);
-
-// Route to add a new item
-// router.post("/items", itemActions.add);
 
 /* ************************************************************************* */
 
