baa30cbfe64b4377916dd5919191f77e081160db
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-05-23 09:42:21 +0200

    css composant cart

--- client/src/components/CartComponent.jsx
+++ client/src/components/CartComponent.jsx
@@ -2,11 +2,13 @@ import "./CartComponent.css"
 
 function CartComponent({game}) {
   return (
-    <section className='cartContainer bgGradient'>
+    <section className='cartContainer bgGradientCard'>
       <img src={game.image} alt={game.title} />
+      <div className="content">
       <h2>{game.title}</h2>
-      <p>Quantité(s) 1</p>
-      <p>Prix {game.price} EUR</p>
+      <h2>Quantité(s) 1</h2>
+      <h2>Prix {game.price} EUR</h2>
+      </div>
     </section>
   )
 }
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,20 +1,25 @@
 import { useLoaderData } from "react-router-dom";
 import CartComponent from "../components/CartComponent";
+import "../components/CartComponent.css";
 
 function Cart() {
 
   const games = useLoaderData()
 
   return (
-    <section className="">
+    <main>
       <h1>Mon panier</h1>
-      <CartComponent
-      game={games[0]}/>
-      <CartComponent
-      game={games[1]}/>
-      <CartComponent
-      game={games[2]}/>
-    </section>
+      <section className="shopContainer">
+        <CartComponent
+        game={games[0]}/>
+        <CartComponent
+        game={games[1]}/>
+        <CartComponent
+        game={games[2]}/>
+        <CartComponent
+        game={games[3]}/>
+      </section>
+    </main>
   );
 }
 
