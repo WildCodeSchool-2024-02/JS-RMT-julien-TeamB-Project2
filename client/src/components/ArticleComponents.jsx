5dfa87c95cbe5b790be55e9530a5536ffd8cb596
Megane <megane.loisele@gmail.com>
2024-05-24 09:42:46 +0200

    ajout du Css et de l'ecriture

--- client/src/components/ArticleComponents.jsx
+++ client/src/components/ArticleComponents.jsx
@@ -1,11 +1,17 @@
+import "./ArticleComponents.css";
+
 function ArticleComponents({ game }) {
   return (
     <section className="articleContainer">
       <img src={game.image} alt={game.id} />
-      <h2>{game.title}</h2>
-      <h4>{game.genre}</h4>
-      <p>{game.description}</p>
-      <h3>{game.price}€</h3>
+      <div className="articleStyle">
+        <h2>{game.title}</h2>
+        <p>Genre: {game.genre}</p>
+        <p>Description: {game.description}</p>
+        <p>Date de Sortie: {game.release_date}</p>
+        <p>Développeur:{game.developer}</p>
+        <p>Prix: {game.price}€</p>
+      </div>
     </section>
   );
 }
