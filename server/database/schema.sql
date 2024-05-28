5829854090c0bafcc942a9fd9e58fbbcf33673b6
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-05-28 17:07:47 +0200

    creation table games avec insert to

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,4 +1,17 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
+create table games (
+  id INT PRIMARY KEY auto_increment NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  image VARCHAR(511) NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  platforms VARCHAR(255) NULL,
+  release_date DATE NOT NULL,
+  developer VARCHAR(255) NOT NULL,
+  publisher VARCHAR(255) NULL,
+  rating VARCHAR(255) NULL,
+  description TEXT NOT NULL,
+  price DECIMAL(5, 2) NOT NULL
 );
+
+
+INSERT INTO games (title, image, genre, platforms, release_date, developer, publisher, rating, description, price) 
+VALUE ("The Witcher 3: Wild Hunt", "https://image.api.playstation.com/vulcan/ap/rnd/202211/0711/kh4MUIuMmHlktOHar3lVl6rY.png", "Action RPG", "PlayStation 4", "2015-05-19", "CD Projekt Red", "CD Projekt", "Mature", "An open-world action RPG set in a dark fantasy universe, featuring rich storytelling, dynamic combat, and vast exploration.", 59.99)
