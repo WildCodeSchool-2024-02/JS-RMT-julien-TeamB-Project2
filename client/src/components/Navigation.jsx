33389c5b325bbbb36836e113f79313e1a8a3b3e9
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-05-28 15:59:02 +0200

    modification Pull request

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 import logoGamingZone from "../assets/images/logoGamingZone.png";
-import logoCategorie from "../assets/images/logoCategorie.png";
+import logoCategory from "../assets/images/logoCategory.png";
 import logoRechercher from "../assets/images/logoRechercher.png";
 import logoPanier from "../assets/images/logoPanier.png";
 
@@ -13,16 +13,28 @@ function Navigation() {
     <nav className="navContainer navBg">
       <ul className="navButtonContainer">
         <Link to="/app">
-          <img className="navLogoSize" src={logoGamingZone} alt="logo" />
+          <img
+            className="navLogoSize"
+            src={logoGamingZone}
+            alt="Logo du site"
+          />
         </Link>
         <Link to="/">
-          <img className="navLogoSize" src={logoCategorie} alt="logo" />
+          <img
+            className="navLogoSize"
+            src={logoCategory}
+            alt="Logo catégorie"
+          />
         </Link>
         <Link to="/articles/:id">
-          <img className="navLogoSize" src={logoRechercher} alt="logo" />
+          <img
+            className="navLogoSize"
+            src={logoRechercher}
+            alt="Logo rechercher"
+          />
         </Link>
         <Link to="/panier">
-          <img className="navLogoSize" src={logoPanier} alt="logo" />
+          <img className="navLogoSize" src={logoPanier} alt="Logo panier" />
         </Link>
       </ul>
     </nav>
