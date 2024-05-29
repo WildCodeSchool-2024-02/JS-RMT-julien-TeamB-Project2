7984f8a0f8fb5657a32ab143ed9bb869bb4d60cd
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-05-29 11:49:22 +0200

    Résolution conflit

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,15 +1,13 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "./GameCard.css";
 
 function GameCard({ game }) {
-  const navigate = useNavigate();
-
   return (
     <li>
-      <button type="button" onClick={() => navigate(`/articles/${game.id}`)}>
+      <Link to={`/articles/${game.id}`}>
         <img className="gameCardImg" src={game.image} alt={game.title} />
-      </button>
+      </Link>
       <div className="gameCardBottom">
         <h2 className="styleTitleH2">{game.title}</h2>
         <p className="styleText">{game.genre}</p>
