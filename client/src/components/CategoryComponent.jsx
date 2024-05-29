3e5148f98cb847af97933fa3c4117d95ec6d75bd
BertrandLrc <bertrand.larroche@hotmail.fr>
2024-05-29 12:03:18 +0200

    creation composant category / CSS composant category (draft)

---
+++ client/src/components/CategoryComponent.jsx
@@ -0,0 +1,29 @@
+import "./CategoryComponent.css";
+import "../App.css";
+
+function Category() {
+  return (
+    <section className="categoryMainContainer">
+      <h1>GENRES</h1>
+      <ul className="categoryContainer">
+        <li>Action RPG</li>
+        <li>Life Simulation</li>
+        <li>RPG</li>
+        <li>Action-Adventure</li>
+        <li>Fighting</li>
+        <li>MOBA</li>
+        <li>Sandbox</li>
+        <li>Battle Royale</li>
+        <li>Sports</li>
+        <li>Hero Shooter</li>
+        <li>Social Deduction</li>
+        <li>Platformer</li>
+        <li>First-Person Shooter</li>
+        <li>Tactical Shooter</li>
+        <li>MMO Shooter</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Category;
--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,29 +1,39 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 import logoGamingZone from "../assets/images/logoGamingZone.png";
 import logoPanier from "../assets/images/logoPanier.png";
+import Category from "./CategoryComponent";
 
 import "../App.css";
 import "./Navigation.css";
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navContainer navBg">
-      <ul className="navButtonContainer">
-        <Link className="noTextDecoration" to="/app">
-          <img className="navLogoSize" src={logoGamingZone} alt="logo" />
-        </Link>
-        <Link className="noTextDecoration" to="/">
-          Catalogue
-        </Link>
-        <Link className="noTextDecoration" to="/article">
-          Article
-        </Link>
-        <Link className="noTextDecoration" to="/panier">
-          <img className="navLogoSize" src={logoPanier} alt="logo" />
-        </Link>
-      </ul>
-    </nav>
+    <>
+      <nav className="navContainer navBg">
+        <ul className="navButtonContainer">
+          <Link className="noTextDecoration" to="/app">
+            <img className="navLogoSize" src={logoGamingZone} alt="logo" />
+          </Link>
+          <Link className="noTextDecoration" to="/">
+            Catalogue
+          </Link>
+          <button type="button" onClick={() => setIsOpen(true)}>
+            Catégories
+          </button>
+          <Link className="noTextDecoration" to="/article">
+            Article
+          </Link>
+          <Link className="noTextDecoration" to="/panier">
+            <img className="navLogoSize" src={logoPanier} alt="logo" />
+          </Link>
+        </ul>
+      </nav>
+      {isOpen && <Category />}
+    </>
   );
 }
 
