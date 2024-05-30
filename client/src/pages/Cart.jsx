d467d17b7aff9c8bd3da574e5809e52089c75e86
Megane <megane.loisele@gmail.com>
2024-05-30 15:00:30 +0200

    ajout d'un bouton vider totalement le panier

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -6,13 +6,16 @@ import "../components/CartComponent.css";
 import "../App.css";
 
 function Cart() {
-  const { cart } = useCart();
+  const { cart, clearCart } = useCart();
   const total = cart.reduce((acc, game) => acc + game.price, 0);
 
   return (
     <main className="bgColorMain">
       <Navigation />
       <h1 className="styleTitleH1">Mon panier</h1>
+      <button type="button" onClick={clearCart}>
+        Vider le panier
+      </button>
       <section className="cartMainContainer">
         {cart.length === 0 ? (
           <p>Votre panier est vide.</p>
