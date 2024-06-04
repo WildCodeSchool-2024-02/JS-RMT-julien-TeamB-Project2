5de295f6be664590dfd42aae4eb09d377548305a
Megane <megane.loisele@gmail.com>
2024-06-04 10:03:24 +0200

    reparation du probleme de branch et donc ajout du css et de la mise en place de la quantité du panier

---
+++ client/src/components/Button/CartButton.jsx
@@ -0,0 +1,16 @@
+import { useCart } from "../../context/CartContext";
+
+function CartButton({ game }) {
+  const { handleGameInCart, isInCart } = useCart();
+  return (
+    <button
+      className="articleButton"
+      type="button"
+      onClick={() => handleGameInCart(game)}
+    >
+      {isInCart(game) ? "Supprimer du Panier" : "Ajouter au panier"}
+    </button>
+  );
+}
+
+export default CartButton;
--- client/src/components/CartComponent.jsx
+++ client/src/components/CartComponent.jsx
@@ -1,24 +1,32 @@
-import "./CartComponent.css";
 import { useCart } from "../context/CartContext";
+import CartButton from "./Button/CartButton";
+import "./CartComponent.css";
 
-function CartComponent({ id, title, image, price }) {
-  const { removeFromCart } = useCart();
-
-  
+function CartComponent({ game }) {
+  const { updateQuantity } = useCart();
   return (
     <section className="cartContainer bgCartContainer">
-      <img className="cartImg" src={image} alt={title} />
+      <img className="cartImg" src={game.image} alt={game.title} />
       <div className="cartContent">
-        <h2 className="styleTitleH2">{title}</h2>
-        <p className="styleText">{price} EUR</p>
-        <button
-          type="button"
-          onClick={() => {
-            removeFromCart(id);
-          }}
-        >
-          Retirer du panier
-        </button>
+        <h2 className="styleTitleH2">{game.title}</h2>
+        <p className="styleText">{game.price} EUR</p>
+        <label>
+          Quantité :{" "}
+          <select
+            name="Quantité"
+            onChange={(event) => updateQuantity(event, game)}
+          >
+            {[...Array(10)].map((_, quantity) => {
+              const key = quantity + 1;
+              return (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <CartButton game={game} />
       </div>
     </section>
   );
