d675700321007976103e936e1e90427798eef5aa
Megane <megane.loisele@gmail.com>
2024-06-04 16:54:38 +0200

    correction des problemes de panier

--- client/src/components/CartComponent.jsx
+++ client/src/components/CartComponent.jsx
@@ -1,20 +1,36 @@
 import "./CartComponent.css";
 import { useCart } from "../context/CartContext";
-
 import addGameIcon from "../assets/images/addGameIcon.png";
 
-function CartComponent({ id, title, image, price }) {
-  const { removeFromCart } = useCart();
+function CartComponent({ game }) {
+  const { updateQuantity, handleGameInCart } = useCart();
 
   return (
     <section className="cartContainer bgCartContainer">
-      <img className="cartImg" src={image} alt={title} />
-        <h2 className="styleTitleH2">{title}</h2>
-        <p className="styleText">{price} EUR</p>
-        <button 
+      <img className="cartImg" src={game.image} alt={game.title} />
+      <div className="cartContent">
+        <h2 className="styleTitleH2">{game.title}</h2>
+        <p className="styleText">{game.price} EUR</p>
+        <label>
+          Quantité :
+          <select
+            name="Quantité"
+            onChange={(event) => updateQuantity(event, game)}
+          >
+            {[...Array(10)].map((_, quantity) => {
+              const key = quantity + 1;
+              return (
+                <option key={key} value={key}>
+                  {key}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+        <button
           type="button"
           onClick={() => {
-            removeFromCart(id);
+            handleGameInCart(game);
           }}
           style={{
             background: "none",
@@ -25,6 +41,7 @@ function CartComponent({ id, title, image, price }) {
         >
           <img src={addGameIcon} alt="Retirer du panier" />
         </button>
+      </div>
     </section>
   );
 }
