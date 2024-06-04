c484fbd4910af525e1ff9f76cd3daa46c8eb072a
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-06-04 11:56:28 +0200

    ajout et ajustement de CSS

--- client/src/components/NewsHome.jsx
+++ client/src/components/NewsHome.jsx
@@ -21,41 +21,19 @@ function NewsCard() {
     fetchData();
   }, []);
 
-  const [expanded, setExpanded] = useState({});
-
-  const toggleExpand = (index) => {
-    setExpanded((prev) => ({
-      ...prev,
-      [index]: !prev[index],
-    }));
-  };
-
   return (
     <section>
       <div className="newsSection">
-        <h2>Actualités des Jeux Vidéo</h2>
+        <h2 className="newsSectionTitle">Actualités des Jeux Vidéo</h2>
         <div className="newsCard">
           {articles.map((article) => (
             <a href={article.url} key={article.url} className="newsLink">
               <div className="newsItem">
                 <h3>{article.title}</h3>
                 <img src={article.urlToImage} alt="PhotoDeL'Article" />
-                <p>{article.author}</p>
+                <p>{article.description.substring(0, 200)}...</p>
                 <p>{new Date(article.publishedAt).toLocaleDateString()}</p>
-                <p>
-                  {expanded[0]
-                    ? article.description
-                    : `${article.description.substring(0, 50)}...`}
-                  <button
-                    type="button"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      toggleExpand();
-                    }}
-                  >
-                    {expanded[50] ? "Moins" : "Plus"}
-                  </button>
-                </p>
+                <p>{article.author}</p>
               </div>
             </a>
           ))}
