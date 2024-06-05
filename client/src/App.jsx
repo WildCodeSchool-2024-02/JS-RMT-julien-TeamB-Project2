5d34fa813aa4127d6e93177ab2d34f9b771c67c4
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-06-05 15:21:38 +0200

    Récupération de la data transmis par main.jsx et transmision par props des données au composant Carousel.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import "./App.css";
+import { useLoaderData } from "react-router-dom";
 import Navigation from "./components/Navigation";
+import Carousel from "./components/Carousel";
 import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  const carouselData = useLoaderData();
   return (
     <main className="bgColorMain">
       <Navigation />
+      <Carousel carouselData={carouselData} />
       <h1 className="styleTitleH1">Bienvenue</h1>
       <Footer />
     </main>
