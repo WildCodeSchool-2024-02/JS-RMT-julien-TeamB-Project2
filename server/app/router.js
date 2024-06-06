e420551d92feddbacae921f0243e5b2749ffa71e
Oresti <hoxhallarioresti02@gmail.com>
2024-06-06 11:36:24 +0200

    Carousel Update

--- server/app/router.js
+++ server/app/router.js
@@ -1,45 +1,41 @@
-const express = require("express");
+const express = require('express');
 
-const games = require("../database/data");
+const games = require('../database/data');
 
 const router = express.Router();
 
-const client = require("../database/client");
+const client = require('../database/client');
 
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
 // Route to get a list of items
-router.get("/games", (req, res) => {
+router.get('/games', (req, res) => {
   if (req.query.genre) {
     res
       .status(200)
       .json(games.filter((game) => game.genre === req.query.genre));
   } else if (req.query.title) {
     const { title } = req.query;
-    const flGames = games.filter((g) =>
-      g.title.toLowerCase().includes(title.toLowerCase())
-    );
+    const flGames = games.filter((g) => g.title.toLowerCase().includes(title.toLowerCase()));
     res.status(200).json(flGames);
   } else {
     res.status(200).json(games);
   }
 });
 
-router.get("/genres", (req, res) => {
+router.get('/genres', (req, res) => {
   client
-    .query("SELECT DISTINCT genre FROM games")
-    .then((genres) =>
-      res.status(200).json(genres[0].map((game) => game.genre))
-    );
+    .query('SELECT DISTINCT genre FROM games')
+    .then((genres) => res.status(200).json(genres[0].map((game) => game.genre)));
 });
 
-router.get("/basket", (req, res) => {
+router.get('/basket', (req, res) => {
   const game = games.slice(0, 6);
   res.status(200).json(game);
 });
 
-router.get("/games/:id", (req, res) => {
+router.get('/games/:id', (req, res) => {
   const foundGame = games.find((game) => game.id === +req.params.id);
   if (foundGame) {
     res.status(200).json(foundGame);
@@ -48,9 +44,9 @@ router.get("/games/:id", (req, res) => {
   }
 });
 
-router.get("/carousel", (req, res) => {
+router.get('/carousel', (req, res) => {
   client
-    .query("SELECT title, image FROM games ORDER BY RAND() LIMIT 6 ")
+    .query('SELECT title, image, id FROM games ORDER BY RAND() LIMIT 6 ')
     .then((dataCarousel) => {
       res.status(200).json(dataCarousel[0]);
     });
