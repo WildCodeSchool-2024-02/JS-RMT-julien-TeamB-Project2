74f6b687243f4ebb0b5e36721e7640d5c8232216
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-06-06 16:54:42 +0200

    Modification route SQL pour le carousel

--- server/app/router.js
+++ server/app/router.js
@@ -1,56 +1,56 @@
-const express = require('express');
+const express = require("express");
 
-const games = require('../database/data');
+const games = require("../database/data");
 
 const router = express.Router();
 
-const client = require('../database/client');
+const client = require("../database/client");
 
 /* ************************************************************************* */
 // Define Your API Routes Here
 /* ************************************************************************* */
 // Route to get a list of items
-router.get('/games', (req, res) => {
+router.get("/games", (req, res) => {
   if (req.query.genre) {
     res
       .status(200)
       .json(games.filter((game) => game.genre === req.query.genre));
   } else if (req.query.title) {
     const { title } = req.query;
-    const flGames = games.filter((g) => g.title.toLowerCase().includes(title.toLowerCase()));
+    const flGames = games.filter((g) =>
+      g.title.toLowerCase().includes(title.toLowerCase())
+    );
     res.status(200).json(flGames);
+  } else if (req.query.type === "carousel") {
+    client
+      .query("SELECT title, image, id FROM games ORDER BY RAND() LIMIT 6")
+      .then((dataCarousel) => {
+        res.status(200).json(dataCarousel[0]);
+      });
   } else {
     res.status(200).json(games);
   }
 });
 
-router.get('/genres', (req, res) => {
+router.get("/genres", (req, res) => {
   client
-    .query('SELECT DISTINCT genre FROM games')
-    .then((genres) => res.status(200).json(genres[0].map((game) => game.genre)));
+    .query("SELECT DISTINCT genre FROM games")
+    .then((genres) =>
+      res.status(200).json(genres[0].map((game) => game.genre))
+    );
 });
 
-router.get('/basket', (req, res) => {
+router.get("/basket", (req, res) => {
   const game = games.slice(0, 6);
   res.status(200).json(game);
 });
 
-router.get('/games/:id', (req, res) => {
-  const foundGame = games.find((game) => game.id === +req.params.id);
-  if (foundGame) {
-    res.status(200).json(foundGame);
-  } else {
-    res.status(200).json({});
-  }
+router.get("/games/:id", (req, res) => {
+  client
+    .query("SELECT * FROM games WHERE id = ?", [req.params.id])
+    .then((result) => res.status(200).json(result[0][0]));
 });
 
-router.get('/carousel', (req, res) => {
-  client
-    .query('SELECT title, image, id FROM games ORDER BY RAND() LIMIT 6 ')
-    .then((dataCarousel) => {
-      res.status(200).json(dataCarousel[0]);
-    });
-});
 /* ************************************************************************* */
 
 module.exports = router;
