ce37804eea9c3898653802eed3007a504ff348d5
Megane <megane.loisele@gmail.com>
2024-06-06 11:49:39 +0200

    Correction minuscule en Majuscule

--- client/src/components/ArticleComponents.jsx
+++ client/src/components/ArticleComponents.jsx
@@ -10,8 +10,8 @@ function ArticleComponents({ game }) {
         <h2 className="styleTitleH2">{game.title}</h2>
         <p className="styleText">Genre: {game.genre}</p>
         <p className="styleText">Description: {game.description}</p>
-        <p className="styleText">release date: {game.release_date}</p>
-        <p className="styleText">developer:{game.developer}</p>
+        <p className="styleText">Release date: {game.release_date}</p>
+        <p className="styleText">Developer:{game.developer}</p>
         <p className="styleText">Price: {game.price}€</p>
         <CartButton game={game} />
       </div>
